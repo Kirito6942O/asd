64febcb8cfced3df414c2a8e039a999589338f93
Rochak Maharjan <maharzanrochak7@gmail.com>
2024-07-24 22:24:22 +0545

    IF else ani switch

--- script/script.js
+++ script/script.js
@@ -1,6 +1,32 @@
-console.log("Hello World")
-document.getElementById("input");
-document.getElementById("button");
+// console.log("Hello World")
+// document.getElementById("input");
+// document.getElementById("button");
+// document.getElementById("word");
 
-button.onclick = function() {
+// button.onclick = function() {
+//     word.textContent = "Your Name is " + input.value;
+//     input.value = "";
+    
+// }
+
+const b = "orange";
+
+if (b == "orange") {
+    console.log("I like oranges");
+} else if (b == "apple") {
+    console.log("i don't like apple");
+} else {
+    console.log("okay");
+}
+
+switch (b) {
+    case "orange":
+        console.log("I like oranges");
+        break;
+    case "apple":
+        console.log("I don't like apple");
+        break;
+    default:
+        console.log("okay");
+        break;
 }
